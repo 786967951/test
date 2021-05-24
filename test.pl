c8725e740634f12080ac4af3670e91022386b6e8
786967951 <786967951@qq.com>
2021-05-24 15:06:24 +0800

    test-teu1

--- test.pl
+++ test.pl
@@ -1,11 +1,11 @@
 #!/data/Software/mydan/perl/bin/perl
 
-system "git tag -n --sort=-taggerdate|grep '^release*'|head -n 2|awk '{print \$1}' > taglist";
-open(DATA, "<taglist");
-chomp(my @tagarr = <DATA>);
-close DATA;
+my $info = `cd ../test && git tag -n |grep '^release*'|head -n 2|awk '{print \$1}'`;
+my @tagarr = split(/\n/,$info);
+my $tagdetail;
 if (@tagarr.length == 2){
-    system "git log --pretty=format:'%h %s  %an' '@tagarr[1]'...'@tagarr[0]' > tagdetail";
-}else  if (@tagarr.length == 1){
-    system "git log --pretty=format:'%h %s  %an' '@tagarr[0]' > tagdetail";
+    $tagdetail = `cd ../test && git log --pretty=format:'%h %s  %an' '@tagarr[1]'...'@tagarr[0]'`;
+}elsif (@tagarr.length == 1){
+    $tagdetail = `cd ../test && git log --pretty=format:'%h %s  %an' '@tagarr[0]'`;
 };
+print $tagdetail;
